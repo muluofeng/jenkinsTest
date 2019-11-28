591fbc162b93685b412edff57c2aa39aa5d99a08
xiexingxing <906877230@qq.com>
2019-11-28 23:17:45 +0800

    init

---
+++ src/main/java/com/example/jenkins/IndexController.java
@@ -0,0 +1,23 @@
+package com.example.jenkins;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author xiexingxing
+ * @Created by 2019-11-28 22:40.
+ */
+@RestController
+@RequestMapping("/test")
+public class IndexController {
+    @RequestMapping("/index")
+    public Object index(){
+        List<Object> objects = new ArrayList<>();
+        objects.add("a");
+        objects.add("b");
+        return objects;
+    }
+}
